92d7b6eb3159fb91de774ee8107ed19d2f1bdf1d
Mario Junior <mario_souza.junior@hotmail.com>
2021-09-26 23:34:17 -0300

    Adicionado TData no select

--- UIBlazorFramework.Web.Client/Pages/Index.razor.cs
+++ UIBlazorFramework.Web.Client/Pages/Index.razor.cs
@@ -1,6 +1,26 @@
-﻿namespace UIBlazorFramework.Web.Client.Pages
+﻿using System.Collections.Generic;
+using UIBlazorFramework.Model.Entities;
+
+namespace UIBlazorFramework.Web.Client.Pages
 {
     public partial class Index
     {
+        public List<OptionItem> SelectData { get; set; }
+
+        public OptionItem SelectedItem { get; set; }
+
+        protected override void OnInitialized()
+        {
+            SelectData = new List<OptionItem>();
+            for(var i = 1; i <= 20; i++)
+            {
+                SelectData.Add(new OptionItem()
+                {
+                    Value = i,
+                    Text = "Option " + i
+                });
+            }
+            base.OnInitialized();
+        }
     }
 }
--- UIBlazorFramework.Web/Components/UISelect.razor.cs
+++ UIBlazorFramework.Web/Components/UISelect.razor.cs
@@ -1,16 +1,78 @@
-﻿namespace UIBlazorFramework.Web.Components
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace UIBlazorFramework.Web.Components
 {
-    public partial class UISelect
+    public partial class UISelect<TData>
     {
+        [Parameter]
+        public IList<TData> Data { get; set; }
+
+        [Parameter]
+        public TData SeletedItem { get; set; }
+
+        [Parameter]
+        public EventCallback<TData> SeletedItemChanged { get; set; }
+
+        [Parameter]
+        public string Value { get; set; }
+
+        [Parameter]
+        public EventCallback<string> ValueChanged { get; set; }
+
+        [Parameter]
+        public Expression<Func<TData, object>> ValueField { get; set; }
+
+        [Parameter]
+        public Expression<Func<TData, object>> TextField { get; set; }
+
         private bool _showContainerOptions;
 
         private string _selectedItemText;
 
         private string _searchText;
 
-        private void OnClickSpan()
+        private string GetText(TData item)
+        {
+            if (item == null)
+                return default;
+
+            var compiledTextField = TextField.Compile();
+            var textFromItem = compiledTextField.Invoke(item);
+
+            return textFromItem.ToString();
+        }
+
+        private string GetValue(TData item)
+        {
+            if (item == null)
+                return default;
+
+            var compiledValueField = ValueField.Compile();
+            var valueFromItem = compiledValueField.Invoke(item);
+
+            return valueFromItem.ToString();
+        }
+
+        private void BindContainerOptions()
         {
             _showContainerOptions = !_showContainerOptions;
+        }
+
+        private void SelectItem(TData item)
+        {
+            SeletedItem = item;
+            SeletedItemChanged.InvokeAsync(item);
+
+            _selectedItemText = GetText(item);
+
+            var value = GetValue(item);
+            Value = value;
+            ValueChanged.InvokeAsync(value);
+            _showContainerOptions = false;
+            _searchText = string.Empty;
         }
     }
 }
